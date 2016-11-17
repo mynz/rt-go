a64b1db89acb0322d4de67c357e5c85cfd833e66
mynz <rollingteapot@gmail.com>
2016-11-17 16:11:29 +0900

    snap

--- main.go
+++ main.go
@@ -7,39 +7,71 @@ import(
 	"image/color"
 	"image/png"
 
-	"./vecmath"
+	// "./vecmath"
+	"github.com/go-gl/mathgl/mgl32"
 )
 
-func writePngFile(filename string) {
-	w, h := 128, 128
-	img := image.NewRGBA(image.Rect(0, 0, w, h))
+func writePngFile(filename string, img image.Image) {
 
-	if true {
-		c := color.RGBA{ 255, 128, 0, 255 }
-		for i := 0; i < w; i++ {
-			img.Set(i, i, c)
-		}
-	}
+/*
+ *     w, h := 128, 128
+ *     img := image.NewRGBA(image.Rect(0, 0, w, h))
+ * 
+ *     if true {
+ *         c := color.RGBA{ 255, 128, 0, 255 }
+ *         for i := 0; i < w; i++ {
+ *             img.Set(i, i, c)
+ *         }
+ *     }
+ */
 
 	fp, err := os.Create(filename)
+	defer fp.Close()
 	if err != nil {
 		panic(err)
 	}
-	defer fp.Close()
 
 	if err := png.Encode(fp, img); err != nil {
 		panic(err)
 	}
 }
 
-// type Foo struct { }
+func RenderImage() *image.RGBA {
+	nx, ny := 200, 100
+
+	img := image.NewRGBA(image.Rect(0, 0, nx, ny))
+
+	for j := ny-1; j >= 0; j-- {
+		for  i := 0; i < nx; i++ {
+			col := mgl32.Vec3{ float32(i) / float32(nx), float32(j) / float32(ny), 0.2 }
+			c := col.Mul(255.99)
+			color := color.RGBA{ uint8(c[0]), uint8(c[1]), uint8(c[2]), 255 }
+			img.Set(i, j, color)
+		}
+	}
+
+	return img
+}
+
 
 func main() {
 	fmt.Println("Hello.")
 
-	fmt.Println("Vec:", vecmath.NewVecZero().Length())
+	img := RenderImage()
+	writePngFile("test.png", img)
 
-	writePngFile("test.png")
+/*
+ *     if false {
+ *         // fmt.Println("Vec:", vecmath.NewVecZero().Length())
+ * 
+ *         v := mgl32.Vec3{1, 2, 3}
+ *         fmt.Println("v0:", v.Len())
+ * 
+ *         fmt.Println("v1:", mgl32.Vec3{4, 5, 6}.Len())
+ * 
+ *         writePngFile("test.png")
+ *     }
+ */
 
 	fmt.Println("Done.")
 }
--- vecmath/vecmath.go
+++ vecmath/vecmath.go
@@ -12,7 +12,6 @@ func NewVecZero() Vec {
 	return Vec{ [3]float32{ 0.0, 0.0, 0.0 } }
 }
 
-
 func (v Vec) Length() float32 {
 	return float32(math.Sqrt(float64(v.V[0] * v.V[0] + v.V[1] * v.V[1] + v.V[2] * v.V[2])))
 }
